551e3f6150eb050f7ffe8b266d6cd4cd2bfb9bc5
Alex Terliuk <alex.terliuk@gmail.com>
2026-04-26 18:43:23 +0300

    feat: add gaps to utils - wraps of SizedBox

---
+++ lib/src/utils/gaps.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Gap12 extends StatelessWidget {
+  const Gap12({super.key});
+
+  @override
+  Widget build(BuildContext context) => SizedBox(height: 12);
+}
+
+class Gap24 extends StatelessWidget {
+  const Gap24({super.key});
+
+  @override
+  Widget build(BuildContext context) => SizedBox(height: 24);
+}
